27c068a1b5a2f5fe313391b025b8e8b57f138f7c
Jerome Wu <jeromewus@gmail.com>
2021-03-02 09:44:05 +0800

    WIP: day 15

--- src/d15.clj
+++ src/d15.clj
@@ -4,17 +4,31 @@
 (def unit-map {"1" [0 1] "2" [0 -1] "3" [-1 0] "4" [1 0]})
 (def dirs ["1" "2" "3" "4"])
 
-(defn find-tank [ctx pos visited]
+(defn find-tank [ctx pos visited status]
   (reduce
     #(let [
-           next-ctx (d05/ic-com (assoc ctx :in (vector %2)))
-            out (last (:out next-ctx))]
+           dir %2
+           ctx' (d05/ic-com (assoc ctx :in (vector dir)))
+           pos' (map + pos (unit-map dir))
+           visited' (conj visited pos)
+           status' (last (:out ctx'))
+           _ (prn dir status' pos pos')]
       (cond
-        (= "0" out) ()
-        (= "1" out) ()
-        (= "2" out) ()))
-    visited
+        (or (= 2 status') (= 2 status)) [ctx' pos' visited' status]
+        (contains? visited pos') [ctx pos visited' status]
+        (= 0 status') [ctx pos visited' status]
+        (= 1 status') (find-tank ctx' pos' visited' status')))
+    [ctx pos visited status]
     dirs))
 
+(defn p1 [filename]
+  (->>
+    (find-tank
+      {:ptr 0 :in [] :out [] :base 0 :prog (d05/read-input filename)}
+      [0 0]
+      #{}
+      1)
+    (second)))
+
 (defn main [args]
-  ())
+  (p1 "data/d15-input.txt"))
