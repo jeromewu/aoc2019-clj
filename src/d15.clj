2127cee4b7428ba926f6b2a2e45f65675fc9c4ad
Jerome Wu <jeromewus@gmail.com>
2021-03-02 13:13:59 +0800

    Complete day 15 part.1

--- src/d15.clj
+++ src/d15.clj
@@ -4,20 +4,45 @@
 (def unit-map {"1" [0 1] "2" [0 -1] "3" [-1 0] "4" [1 0]})
 (def dirs ["1" "2" "3" "4"])
 
-(defn find-tank [ctx pos visited status]
+(defn find-tank [ctx pos visited steps status]
   (reduce
     #(let [
+           [ctx pos visited steps status] %1
            dir %2
            ctx' (d05/ic-com (assoc ctx :in (vector dir)))
            pos' (map + pos (unit-map dir))
            visited' (conj visited pos)
            status' (last (:out ctx'))
-           _ (prn dir status' pos pos')]
+           steps' (inc steps)]
       (cond
-        (or (= 2 status') (= 2 status)) [ctx' pos' visited' status]
-        (contains? visited pos') [ctx pos visited' status]
-        (= 0 status') [ctx pos visited' status]
-        (= 1 status') (find-tank ctx' pos' visited' status')))
+        (or (contains? visited' pos') (= 0 status')) [ctx pos visited' steps status]
+        (= 2 status') (reduced [ctx' pos' visited' steps' status'])
+        (= 1 status')
+        (let [
+              ret (find-tank ctx' pos' visited' steps' status')
+              status'' (last ret)
+              visited'' (nth ret 2)]
+          (if (= 2 status'')
+            (reduced ret)
+            [ctx pos visited'' steps status]))))
+    [ctx pos visited steps status]
+    dirs))
+
+(defn get-tiles [ctx pos visited status]
+  (reduce
+    #(let [
+           [ctx pos visited status] %1
+           dir %2
+           ctx' (d05/ic-com (assoc ctx :in (vector dir)))
+           pos' (vec (map + pos (unit-map dir)))
+           visited' (assoc visited pos status)
+           status' (last (:out ctx'))]
+      (cond
+        (or (contains? visited' pos') (= 0 status')) [ctx pos (assoc visited' pos' status') status]
+        :else
+        (let [
+              ret (get-tiles ctx' pos' visited' status')]
+          [ctx pos (nth ret 2) status])))
     [ctx pos visited status]
     dirs))
 
@@ -27,8 +52,19 @@
       {:ptr 0 :in [] :out [] :base 0 :prog (d05/read-input filename)}
       [0 0]
       #{}
+      0
       1)
-    (second)))
+    (#(nth % 3))))
+
+(defn p2 [filename]
+  (->>
+    (get-tiles
+      {:ptr 0 :in [] :out [] :base 0 :prog (d05/read-input filename)}
+      [0 0]
+      {}
+      1N)
+    (#(nth % 2))))
 
 (defn main [args]
-  (p1 "data/d15-input.txt"))
+  (prn (p1 "data/d15-input.txt"))
+  (prn (p2 "data/d15-input.txt")))
