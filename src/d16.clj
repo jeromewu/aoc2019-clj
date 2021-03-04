b886e1397b93be7ddc6612a8fa5be445ef9dbac0
Jerome Wu <jeromewus@gmail.com>
2021-03-04 22:48:02 +0800

    WIP: day 16

---
+++ src/d16.clj
@@ -0,0 +1,64 @@
+(ns d16)
+
+(def base [0 1 0 -1])
+
+(defn read-input [filename]
+  (->>
+    (slurp filename)
+    (re-seq #"\d")
+    (map #(Integer/parseInt %))
+    (vec)))
+
+(defn rightmost [n]
+  (mod (max n (- n)) 10))
+
+(def get-pattern
+  (memoize
+    (fn [len n-repeat]
+      (let [len+ (inc len)]
+        (reduce
+          #(let [pattern (concat %1 (repeat n-repeat (nth base (mod %2 (count base)))))]
+             (if (>= (count pattern) len+)
+               (reduced (drop 1 (take len+ pattern)))
+               pattern))
+          []
+          (range len+))))))
+
+(defn fft [phase n-phases digits]
+  (prn phase)
+  (let [len (count digits)]
+    (if (= phase n-phases)
+      digits
+      (->>
+        (map
+          #(let [pattern (get-pattern len (inc %))]
+            (->>
+              (map * digits pattern)
+              (reduce +)
+              (rightmost)))
+          (range len))
+        (recur (inc phase) n-phases)))))
+
+(defn p1 [filename n-phases]
+  (let [digits (read-input filename)]
+    (->>
+      (fft 0 n-phases digits)
+      (take 8)
+      (reduce str))))
+
+(defn p2 [filename n-phases]
+  (let [digits (read-input filename)
+        digits-repeated (reduce into (repeat 10000 digits))
+        offset (Integer/parseInt (reduce str (take 7 digits)))
+        _ (prn "digits count = " (count digits-repeated))
+        _ (prn "offset =" offset)]
+    (->>
+      (fft 0 n-phases digits-repeated)
+      (drop offset)
+      (take 8)
+      (reduce str))))
+    
+
+(defn main [args]
+  ; (prn (p1 "data/d16-input.txt" 100))
+  (prn (p2 "data/d16-input2.txt" 100)))
